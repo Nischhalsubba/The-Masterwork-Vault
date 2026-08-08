8a2d4d8953504da89caae1a60a0e88f50f9ebb13
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-08 07:53:09 +0545

    Combine direct icon data map

---
+++ src/data/iconData.ts
@@ -0,0 +1,40 @@
+import c0 from './icons/iconData0'
+import c1 from './icons/iconData01'
+import c2 from './icons/iconData02'
+import c3 from './icons/iconData03'
+import c4 from './icons/iconData04'
+import c5 from './icons/iconData05'
+import c6 from './icons/iconData06'
+import c7 from './icons/iconData07'
+import c8 from './icons/iconData08'
+import c9 from './icons/iconData09'
+import c10 from './icons/iconData10'
+import c11 from './icons/iconData11'
+import c12 from './icons/iconData12'
+import c13 from './icons/iconData13'
+import c14 from './icons/iconData14'
+import c15 from './icons/iconData15'
+import c16 from './icons/iconData16'
+
+const iconData: Record<string, string> = Object.assign(
+  {},
+  c0,
+  c1,
+  c2,
+  c3,
+  c4,
+  c5,
+  c6,
+  c7,
+  c8,
+  c9,
+  c10,
+  c11,
+  c12,
+  c13,
+  c14,
+  c15,
+  c16,
+)
+
+export default iconData
