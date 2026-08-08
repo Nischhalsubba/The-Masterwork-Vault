2944154b60997d3f658ad95fc5594e7138f9bc8e
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-08 15:11:11 +0545

    Load workbench accessibility overrides

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './drilldown.css'
 import './features.css'
+import './features-a11y.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
