573e8dd492708a5aacb262eea9228653158f3eb8
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-08 22:11:36 +0545

    Load overlay fixes after native polish

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import './stats-drawer.css'
 import './mobile-native.css'
 import './mobile-browser-fixes.css'
 import './native-app-polish.css'
+import './native-overlay-fixes.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
