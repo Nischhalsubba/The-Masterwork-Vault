45daa433e642773ad02957abd4677f9818c4a69f
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-09 10:09:36 +0545

    Add missing gathering material icons

---
+++ src/data/gatheringMaterialIconOverrides.ts
@@ -0,0 +1,9 @@
+const gatheringMaterialIconOverrides: Record<string, string> = {
+  'sugar-beet': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/c/cf/Crafting_Resource_Sugarbeat.png/revision/latest',
+  'aberrant-blood': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/a/a1/Crafting_Resource_Aberrantblood.png/revision/latest',
+  'aberrant-bone': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/e/e7/Crafting_Resource_Bone_Aberrant.png/revision/latest',
+  'chamomile': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/9/97/Crafting_Resource_Chamomile.png/revision/latest',
+  'beast-horn': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/5/53/Crafting_Resource_Beasthorn.png/revision/latest',
+}
+
+export default gatheringMaterialIconOverrides
