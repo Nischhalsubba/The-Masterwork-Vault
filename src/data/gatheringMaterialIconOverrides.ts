a13d074274c8388969e67981492546d22e901fa5
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-09 10:22:13 +0545

    Use stable profession material icon redirects

--- src/data/gatheringMaterialIconOverrides.ts
+++ src/data/gatheringMaterialIconOverrides.ts
@@ -1,9 +1,12 @@
+const fandomFile = (fileName: string) =>
+  `https://neverwinter.fandom.com/wiki/Special:Redirect/file/${encodeURIComponent(fileName)}`
+
 const gatheringMaterialIconOverrides: Record<string, string> = {
-  'sugar-beet': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/c/cf/Crafting_Resource_Sugarbeat.png/revision/latest',
-  'aberrant-blood': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/a/a1/Crafting_Resource_Aberrantblood.png/revision/latest',
-  'aberrant-bone': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/e/e7/Crafting_Resource_Bone_Aberrant.png/revision/latest',
-  'chamomile': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/9/97/Crafting_Resource_Chamomile.png/revision/latest',
-  'beast-horn': 'https://static.wikia.nocookie.net/neverwinter_gamepedia/images/5/53/Crafting_Resource_Beasthorn.png/revision/latest',
+  'sugar-beet': fandomFile('Crafting_Resource_Sugarbeat.png'),
+  'aberrant-blood': fandomFile('Crafting_Resource_Aberrantblood.png'),
+  'aberrant-bone': fandomFile('Crafting_Resource_Bone_Aberrant.png'),
+  'chamomile': fandomFile('Crafting_Resource_Chamomile.png'),
+  'beast-horn': fandomFile('Crafting_Resource_Beasthorn.png'),
 }
 
 export default gatheringMaterialIconOverrides
