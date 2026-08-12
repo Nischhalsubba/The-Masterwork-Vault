2c60bcf9a4db7575622613fba0f6518857207e6c
Nischhal Raj Subba <hinischalsubba@gmail.com>
2026-08-12 10:08:35 +0545

    Load typography system after component styles

--- src/main.tsx
+++ src/main.tsx
@@ -21,6 +21,7 @@ import './journey-page.css'
 import './icon-fallbacks.css'
 import './quality-fixes.css'
 import './masterwork-next.css'
+import './typography.css'
 
 const App = lazy(() => import('./App'))
 const MobileV4Shell = lazy(() => import('./components/MobileV4Shell').then((module) => ({ default: module.MobileV4Shell })))
